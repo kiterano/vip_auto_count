1c51c6a73a93b7d05c8f02824a8b15f1d0b2ea46
kiterano <88527218+kiterano@users.noreply.github.com>
2022-04-23 17:58:02 +0900

    Update vipcount.py

--- vipcount.py
+++ vipcount.py
@@ -53,7 +53,7 @@ tools = pyocr.get_available_tools()
 tool = tools[0]
 builder = pyocr.builders.TextBuilder()
 
-cam_id = 1 # 自分の仮想カメラのデバイスID (大体0～3)
+cam_id = 0 # 自分の仮想カメラのデバイスID (大体0～3)
 cap = cv2.VideoCapture(cam_id)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920) # カメラ画像の横幅を1920に設定
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) # カメラ画像の縦幅を1080に設定
@@ -61,7 +61,7 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) # カメラ画像の縦幅を1080に設
 win_image = cv2.imread('win.png') # 自分が勝ったときのリザルト
 lose_image = cv2.imread('lose.png') #　自分が負けたときのリザルト
 
-print(cap.isOpened())
+# print(cap.isOpened())
 
 # if(os.path.exists('vipc.txt')): #もしテキストファイルが存在していれば、読み込んで現在の連勝数と戦闘力を表示
 #     print(get_wins())
@@ -70,8 +70,6 @@ print(cap.isOpened())
 # else: #もしテキストファイルが存在しなければ、新しくテキストファイルを作り0連勝を書き込む
 #     reset_wins()
 #     print('0連勝')
-
-print(get_wins())
 
 win_is_counted: bool = False
 first_rate_is_counted: bool = False
@@ -87,6 +85,8 @@ while True:
     if not first_rate_is_counted: # 最初に戦闘力を取得する
         first_rate = read_first_rate_from_image(frame)
         write_rate(first_rate)
+        print(True)
+        print(get_wins())
         print(get_rate())
         first_rate_is_counted = True
         
